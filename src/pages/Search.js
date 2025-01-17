b06389ecb1cc71663ee50c5e3bd508fc2cbe789b
arielclarence <ariel.clarence.t@gmail.com>
2025-01-17 12:04:09 +0100

    variable update

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -6,14 +6,13 @@ const SearchPage = () => {
     const [query, setQuery] = useState("");
     const [results, setResults] = useState([]);
     const [error, setError] = useState("");
-    const ingredientServiceUrl = process.env.INGREDIENT_SERVICE_URL;
     
     const handleSearch = async (e) => {
         e.preventDefault();
         setError("");
 
         try {
-            const response = await axios.get("${ingredientServiceUrl}/ingredients/search", {
+            const response = await axios.get("http://localhost:8081/ingredients/search", {
                 params: { query, size: 1, page: 0 },
             });
 
